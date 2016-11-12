5dcc0a04df206ed0ddb20245c4dc8558ecbc284e
royang2012@hotmail.com <Cc11910108>
2016-11-11 21:20:46 -0500

    - process_data.py is updated. Now every column that needs pre-processing is included.

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime as dt
+import numpy as np
 import math
 train_df = pd.read_csv('./input/train_batch.csv')
 def date_to_span(date_string):
@@ -10,19 +11,27 @@ def date_to_span(date_string):
 
 indrel_rescale = lambda x: 1 if 1 else 2
 
+# # There is no need to fill this column as it has already been deleted by filtering
+# ind_empleado_fill = lambda x: 'P' if np.isnan
+
 sexo_fill = lambda x: 'H' if (x!='H')&(x!='V') else x
 
 antiguedad_rescale = lambda x: -1 if -999999 else x
 
+indrel_1mes_fill = lambda  x: 0 if np.isnan(x) else x
+
+tiprel_1mes_fill = lambda  x: 'I' if (x!='P')&(x!='I')&(x!='A')&(x!='R') else x
+
 conyuemp_fill = lambda x: 'N' if (x!='S')&(x!='N') else x
 
-canal_entrada_fill = lambda x: 'unknown' if x.isnull() else x
+# canal_entrada_fill = lambda x: 'unknown' if x.isnull() else x
 
-cod_prov_NA = lambda x: 0 if 'NA' else x
+# somehow the empty cell is called 'NA'
+cod_prov_fill = lambda x: 0 if 'NA' else x
 
 renta_fill = lambda x: 101490.5 if 'NA' else x
 
-segmento_fill = lambda x: '02 - PARTICULARES' if math.isnan(x) else x
+segmento_fill = lambda x: '02 - PARTICULARES' if 'NA' else x
 
 # fill blanks in 'sexo' with 'H'
 train_df['sexo'].map(sexo_fill)
@@ -30,8 +39,9 @@ train_df['sexo'].map(sexo_fill)
 # scale -99999 in 'antiguedad' to -1
 train_df['antiguedad'].map(antiguedad_rescale)
 
+# # this column is replaced by antiguedad
 # convert 'first date in contract' into time span
-train_df['contract_history'] = train_df.fecha_alta.apply(date_to_span)
+# train_df['contract_history'] = train_df.fecha_alta.apply(date_to_span)
 del train_df['fecha_alta']
 
 # convert '99' in 'indrel' to 2
@@ -39,16 +49,26 @@ train_df['indrel'].map(indrel_rescale)
 
 # 'ult_fec_cli_1t', 'indrel_1mes' are deleted
 del train_df['ult_fec_cli_1t']
-del train_df['indrel_1mes']
+# del train_df['indrel_1mes']
+
+# fill blanks in indrel_1mes
+train_df['indrel_1mes'].map(indrel_1mes_fill)
+
+# fill blanks in tiprel_1mes
+train_df['tiprel_1mes'].map(tiprel_1mes_fill)
 
 #  fill blanks in 'conyuemp'
 train_df['conyuemp'].map(conyuemp_fill)
 
-#  fill blanks in 'canal_entrada'
-train_df['canal_entrada'].map(canal_entrada_fill)
+# # delete this column because there are too many different categories
+# train_df['canal_entrada'].map(canal_entrada_fill)
+del train_df['canal_entrada']
 
 #  replace 'NA' in 'tipodom' with 0
-train_df['cod_prov'].map(cod_prov_NA)
+train_df['cod_prov'].map(cod_prov_fill)
+
+# name of the province is deleted
+del train_df['nomprov']
 
 # fill income with median
 train_df['renta'].map(renta_fill)
