e963ed93d1cbc5634022325f996734af287ede22
royang2012@hotmail.com <Cc11910108>
2016-11-18 19:31:29 -0500

    - generated a new table called santander_train7 in santander.db. The new table contains only customers whose records are complete and continuous over 7 months span. If the time continuity breaks, time points afterward is abandoned.

--- sqliteDBSetup/generate7MonthDB.py
+++ sqliteDBSetup/generate7MonthDB.py
@@ -1,7 +1,7 @@
 import sqlite3 as sql
 import pandas as pd
 from tqdm import tqdm
-connectionPath = "./santander_data.db"
+connectionPath = "../santander_data.db"
 santanderCon = sql.connect(connectionPath)
 
 table_name = 'santander_train'
@@ -11,26 +11,45 @@ table_name = 'santander_train'
 select_statement = "select * from santander_train strain order by ncodpers, fecha_dato DESC"
 train_df = pd.read_sql(select_statement, santanderCon)
 
-train_list = []
-date_list = ['2016-05-28', '2016-04-28', '2016-03-28', '2016-02-28', '2016-01-28', '2015-12-28', '2015-11-28']
+# train_df.drop(train_df.index[8], inplace=1)
+# train_df.drop(train_df.index[18], inplace=1)
+# train_df.set_value(8, 'fecha_dato', '123')
+# train_df.set_value(18, 'fecha_dato', '123')
 
+
+# train_list = []
+date_list = ['2016-05-28', '2016-04-28', '2016-03-28', '2016-02-28', '2016-01-28', '2015-12-28', '2015-11-28',
+             '2015-10-28', '2015-09-28', '2015-08-28', '2015-07-28', '2015-06-28', '2015-05-28', '2015-04-28',
+             '2015-03-28', '2015-02-28', '2015-01-28']
 last_idx = 15889
 month_count = 0
-date_count = 1
+date_count = 1  # current index in data_list
+over7 = 0   # tag if there are over 7 months continuous data
+appended = 0
 for i in tqdm(range(1, train_df.shape[0])):
     if train_df.ix[i].ncodpers == last_idx:
         current_date = date_list[date_count]
         if train_df.ix[i].fecha_dato == current_date:
             month_count += 1
             if month_count == 6:
-                # month_count = 0
-                if train_df.ix[i-6].fecha_dato == date_list[0]:
-                    train_list.append(train_df.ix[i-6:i])
+                over7 = 1
+            if train_df.ix[i].fecha_dato == '2015-01-28':
+                current_customer = train_df.ix[i-month_count:i, 2:]
+                current_customer.to_sql('santander_train7', santanderCon, if_exists='append')
+                appended = 1
+        else:
+            if over7 == 1:
+                if appended == 0:
+                    current_customer = train_df.ix[i-month_count:i-1, 2:]
+                    current_customer.to_sql('santander_train7', santanderCon, if_exists='append')
+                    appended = 1
     else:
         date_count = 0
         month_count = 0
+        over7 = 0
+        appended = 0
     last_idx = train_df.ix[i].ncodpers
     date_count += 1
-test_6_df = pd.concat(train_list)
-
-test_6_df.to_sql('santander_train7', santanderCon, if_exists='append')
+# test_6_df = pd.concat(train_list)
+#
+# test_6_df.to_sql('santander_train7', santanderCon, if_exists='append')
