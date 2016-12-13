17010db2be5155b5ffd12bc806ae8a8900f4de12
royang2012@hotmail.com <Cc11910108>
2016-12-13 11:04:17 -0500

    - Problem solved! submission generation was wrong! BY using the correct submission generation function, score improved to 0.0260411

--- fromKaggle/less_is_more_my_sub_gen.py
+++ fromKaggle/less_is_more_my_sub_gen.py
@@ -455,23 +455,3 @@ if __name__ == "__main__":
     merged_df.ix[null_list[0], 'added_products'] = high_frequency_products
     merged_df.to_csv('../output/sub_161212_my_sub_gen.csv', index=False)
 
-    # kaggle submission generation
-    target_cols = np.array(train_fea_df.columns[16:])
-    preds_s = np.argsort(preds, axis=1)
-    preds_s = np.fliplr(preds_s)[:, :7]
-    final_preds = [" ".join(list(target_cols[pred])) for pred in preds_s]
-    predicted_df = pd.DataFrame({'ncodpers': pred_fea_df6.ncodpers.values
-                                 , 'added_products': final_preds})
-    # out_df = pd.DataFrame({'ncodpers': test_id, 'added_products': final_preds})
-    # out_df.to_csv('sub_xgb_new.csv', index=False)
-    # print(datetime.datetime.now() - start_time)
-    test_idx_df = pd.read_sql("select ncodpers from santander_test order by ncodpers", santanderCon)
-    # pred_idx_df = pred_fea_df6.reset_index(drop=True)
-    merged_df = pd.merge(test_idx_df, predicted_df, how='left', on='ncodpers')
-    high_frequency_products = "ind_cco_fin_ult1 ind_cno_fin_ult1 ind_ecue_fin_ult1 " \
-                              "ind_ecue_fin_ult1 ind_nomina_ult1 ind_nom_pens_ult1 " \
-                              "ind_recibo_ult1"
-    null_list = np.where(merged_df.added_products.isnull().values==1)[0]
-    merged_df.ix[null_list[0], 'added_products'] = high_frequency_products
-    file_name = '../output/sub_161211_their_sub_binary_xgb.csv'
-    merged_df.to_csv(file_name, index=False)
